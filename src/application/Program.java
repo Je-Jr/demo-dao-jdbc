0f0aecee1960e2e9cbb7cf98757e9398407e0964
Jorge Junior <jr.jorge500@yahoo.com>
2024-12-26 20:00:28 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted new id = " + newSeller.getId());
+
     }
 }
